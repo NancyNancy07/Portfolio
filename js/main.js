005f19e23465b95d7e5abb138d54f0ece4b9580d
NancyNancy07 <148770109+NancyNancy07@users.noreply.github.com>
2023-11-20 10:31:26 +0100

    adding light mode

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const icon = document.querySelector("#icon");
+icon.addEventListener("click", function (event) {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
+    icon.src = "/images/moon.png";
+  } else {
+    icon.src = "/images/sun.png";
+  }
+});
