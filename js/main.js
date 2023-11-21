5249cc61d807092557414ca20a7933a98d116c7d
NancyNancy07 <148770109+NancyNancy07@users.noreply.github.com>
2023-11-21 20:13:21 +0100

    added eventlisteners

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// changing theme
 const icon = document.querySelector("#icon");
 icon.addEventListener("click", function (event) {
   document.body.classList.toggle("light-theme");
@@ -5,5 +6,22 @@ icon.addEventListener("click", function (event) {
     icon.src = "images/moon.png";
   } else {
     icon.src = "images/sun.png";
+  }
+});
+// user input
+const headingElement = document.querySelector("#heading");
+const subHeadingElement = document.querySelector("#sub-heading");
+const inputElement = document.querySelector("#input-name");
+const buttonElement = document.querySelector("#submit-button");
+
+buttonElement.addEventListener("click", function (event) {
+  event.preventDefault();
+  let enteredName = inputElement.value;
+  if (enteredName !== "") {
+    headingElement.innerText = "Thank You!" + " " + enteredName;
+    subHeadingElement.innerText = "For Contacting Me...";
+  } else {
+    headingElement.innerText = "Contact Me";
+    subHeadingElement.innerText = " Fill this Form to Contact";
   }
 });
